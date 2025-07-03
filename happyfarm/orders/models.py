a61e95a6f9261759a8ac0d65e163157953126f0c
NoteAnywhere <noteanywhere@example.com>
2025-07-03 16:52:53 +0900

    feat: create order model

--- happyfarm/orders/models.py
+++ happyfarm/orders/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+# orders/models.py
 
-# Create your models here.
+from django.db import models
+from users.models import CustomUser
+from products.models import Product
+
+class Order(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='orders')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    ordered_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} 주문 - {self.product.name} x{self.quantity}"
