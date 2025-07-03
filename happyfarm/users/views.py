d3fcc03c6f8e8f2eefecae15e5078fe2b1a63af4
NoteAnywhere <noteanywhere@example.com>
2025-07-03 12:53:58 +0900

    feat: view 작성

--- happyfarm/users/views.py
+++ happyfarm/users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import CustomUser
+from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
