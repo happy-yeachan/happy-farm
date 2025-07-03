910877a2b1903b85e72356151dda172892816e44
NoteAnywhere <noteanywhere@example.com>
2025-07-03 13:00:49 +0900

    feat: 회원가입 뷰 생성

--- happyfarm/users/views.py
+++ happyfarm/users/views.py
@@ -1,9 +1,15 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from .models import CustomUser
 from .serializers import UserSerializer
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+class SignUpView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
