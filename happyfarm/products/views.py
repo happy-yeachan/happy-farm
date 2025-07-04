dd3abc2810c1711795d0435d0bcee449a77321e2
NoteAnywhere <noteanywhere@example.com>
2025-07-04 11:22:11 +0900

    feat: 판매자 권한 로직 추가

--- happyfarm/products/views.py
+++ happyfarm/products/views.py
@@ -1,9 +1,15 @@
 # products/views.py
 
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Product
 from .serializers import ProductSerializer
+from .permissions import IsSellerOrReadOnly
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [IsSellerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)  # 상품 등록 시 셀러 연결
